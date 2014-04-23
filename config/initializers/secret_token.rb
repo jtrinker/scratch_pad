e6c501d962657c29cd052780a245cf395940b2f6
Jameson Trinker <jameson.trinker@gmail.com>
2014-04-23 10:43:39 -0500

    initial commit, backbone/rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScratchPad::Application.config.secret_key_base = '466d938867b7b0267ac778d69562304eceec6b080d7cedcd615ad0989e2b0f44a2f05c1425cd3a244292dec2ba94b7a0c8f95d539b22ff6a74188c0499b27cb8'
